44b20585445d363198551461bb847721af9f43ee
Nicolas Nocete <nicolasnocete@gmail.com>
2024-11-21 15:35:51 -0300

    add hitos

--- data/hitos.jsx
+++ data/hitos.jsx
@@ -1,46 +1,90 @@
+import { type } from "os";
+
 export const hitos = [
   {
     id: 0,
-    date: "1949",
-    title: "1949 - Establecimiento de la Educación Universitaria Gratuita",
-    content:
-      "En 1949, el gobierno argentino estableció la educación universitaria gratuita, permitiendo un mayor acceso a la educación superior para todos los ciudadanos.",
-    link: "https://www.youtube.com/embed/X1p3oQRXEcI",
+    date: 1918,
+    title: "Reforma Universitaria de 1918",
+    content: `La Reforma Universitaria de 1918 estuvo caracterizada por el anhelo de la reforma social y de un espíritu democratizador. Hasta ese momento accedían a la universidad sólo los hombres de la élite Argentina. Los reformistas lograron establecer el cogobierno, la autonomía, los concursos docentes, la extensión y la libertad de cátedra como principios fundamentales, Sin bien la gratuidad fue un pedido de los estudiantes,  no fue tomado por el poder ejecutivo. 
+      Durante los años 30 y 40 distintas agrupaciones estudiantiles, docentes, culturales y políticas cuestionaron el carácter arancelado y elitista del ingreso a las instituciones públicas y militaron activamente para democratizar el acceso a la educación superior.
+    `,
+    link: "https://difusion.pci.unc.edu.ar/unc/2018/reforma-universitaria/images/generales_reformistas_04_b.jpg",
+    type: "image",
   },
   {
     id: 1,
-    date: "1950",
-    title: "Años 1950 - Expansión de Universidades",
-    content:
-      "Durante los años 1950, se establecieron varias nuevas universidades en toda Argentina, ampliando el acceso a la educación superior.",
-    link: "https://www.youtube.com/embed/ffV2HwO5LHg",
+    date: "1949",
+    title: "Sanción de la gratuidad universitaria en Argentina",
+    content: `En 1949, el gobierno argentino estableció la educación universitaria gratuita, permitiendo un mayor acceso a la educación superior para toda la ciudadanía.
+      El Presidente Juan Domingo Perón veía fundamental el acceso de los hijos de los obreros a la universidad para su capacitación e inserción laboral en las industrias fundamentales de la Argentina. 
+      Un año antes, en agosto de 1948, se creaba la Universidad Obrera Nacional con el objetivo de formar profesionales de origen obrero para la industria nacional y mejorar su movilidad social.
+      La gratuidad universitaria se vió reflejada rápidamente en la matrícula, pasó de 40.284 alumnos en 1945 a 138.871 en 1955. `,
+    link: "https://images.pagina12.com.ar/styles/focal_3_2_700x467/public/2024-11/891741-4a-0.jpeg?h=b69e0e0e&itok=YSQm0Bix",
+    type: "image",
   },
   {
     id: 2,
-    date: "1970",
-    title: "Años 1970 - Movimientos Estudiantiles",
-    content:
-      "Los años 1970 vieron significativos movimientos estudiantiles que abogaban por reformas educativas y mayores derechos estudiantiles.",
+    date: "1950",
+    title: "La Libertadora y los gobiernos civiles débiles (1955 -1966)",
+    content: `En este período se restringieron muchos de los postulados de la reforma universitaria y se impusieron los exámenes de ingreso. El cobro de aranceles y la eliminación del acceso irrestricto redujeron las posibilidades de acceso a los estudios superiores. En esta etapa de la historia argentina se expulsaron a los docentes que estaban vinculados al Peronismo y se reprimieron estudiantes. La “Noche de los bastones largos” representa un hito fundamental de la lucha estudiantil en 1966. Se movilizaron contra el Decreto 16912 que buscaba poner fin a más de 40 años de autonomía universitaria, cogobierno estudiantil y libertad de cátedra. `,
+    link: "https://scontent.fcor2-2.fna.fbcdn.net/v/t1.6435-9/38137337_1759951557386539_6335999857446092800_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEBuhY8cC80KZ1Wr9KwkOfnaSlCf8ix9UtpKUJ_yLH1S7rUqfz8OcbhT2M8OF_wTQI&_nc_ohc=VYHo15oq4TUQ7kNvgHOgz-W&_nc_zt=23&_nc_ht=scontent.fcor2-2.fna&_nc_gid=Ajbg6PFHLneaFfccx-C6Vbb&oh=00_AYCokaskbLKSb0_kAK6tvXutkhSmJF5LFJuYpOncpymvPA&oe=6766E785",
+    type: "image",
   },
   {
     id: 3,
-    date: "1983",
-    title: "1983 - Retorno a la Democracia",
-    content:
-      "Con el retorno a la democracia en 1983, hubo un renovado enfoque en mejorar la calidad y accesibilidad de la educación universitaria.",
+    date: "1970",
+    title: "Tercer gobierno de Perón y el Estado Terrorista",
+    content: `En el tercer  gobierno de Juan Domingo Perón , se sancionó la Ley No 20.654 de Universidades Nacionales, conocida como Ley Taiana en honor al entonces Ministro de Educación que la impulsó. Sus aspectos principales incluían la libertad de cátedra, el cogobierno entre estudiantes, docentes y no docentes, la gratuidad, la autonomía y autarquía universitaria; lo que se condice con una reivindicación de los principios reformistas.
+      Con la muerte de Perón y la asunción de su esposa Isabel Martinez de Perón, se intervinieron las universidades. Los nuevos interventores asumieron escoltados por grupos militares y parapoliciales e implementaron una política de control, persecución y miedo. 
+      Obligaron a los centros de estudiantes a cerrar, expulsaron a docentes y estudiantes, restauraron el ingreso por examen y cupo, e intentaron modificar la ley de universidades para que el Poder Ejecutivo pudiera nombrar a las autoridades.`,
+    link: "https://www.infobae.com/resizer/v2/T2CDSKMKABCHLKZQEMTAJDNYQM.jpg?auth=29d3c73db2ab1ad8a2e152bba4c04054ce73f9293a00ab5e79a12dc3d0c976ea&smart=true&width=992&height=558&quality=85",
+    type: "image",
   },
   {
     id: 4,
-    date: "2000",
-    title: "Años 2000 - Avances Tecnológicos",
-    content:
-      "Los años 2000 trajeron avances tecnológicos que transformaron el panorama educativo, incluyendo la introducción de plataformas de aprendizaje en línea.",
+    date: "1976",
+    title: "Golpe de Estado y Dictadura Militar",
+    content: `El 24 de marzo de 1976 se produce en Argentina el último -y más violento- golpe de Estado. La dictadura del ‘76 gestionó la muerte de miles de cientos de personas a través de una maquinaria de centros clandestinos de detención, tortura, violación y muerte. Las víctimas del terrorismo de Estado fueron en su mayoría estudiantes, obreros, militantes políticos, personas del colectivo de la diversidad y cualquier persona considerada “subersiva”.	
+      Se intervinieron las universidades públicas, ya que se consideraban un foco estratégico de la subversión. Los docentes y directivos se designaban sin concursos y debían responder al Poder Ejecutivo, en muchos casos se realizaban “listas negras” con nombres de estudiantes y docentes que luego eran encarcelados de manera ilegal y posteriormente asesinados. 
+      Las modificaciones al sistema universitario se dirigieron igualmente al control de la matrícula a través de determinados requisitos de ingreso. Se instauró una política de admisión basada en un sistema con doble barrera: se debía aprobar un examen de ingreso obligatorio y, luego entrar dentro de un cupo limitado que se habilitaba cada año para las carreras. Esto impactó de manera directa en un descenso del número de estudiantes universitarios, disminuyendo la cantidad de estudiantes de 487.389 a 386.743 al finalizar la dictadura.`,
+    link: "https://cultura.amia.org.ar/wp-content/uploads/2021/07/Noche-de-los-lapices.jpg",
+    type: "image",
   },
   {
     id: 5,
-    date: "2020",
-    title: "2020 - Respuesta al COVID-19",
+    date: "1983",
+    title: "Retorno a la Democracia",
     content:
-      "En respuesta a la pandemia de COVID-19, las universidades se adaptaron rápidamente al aprendizaje remoto, asegurando la continuidad de la educación.",
+      "Con el retorno a la democracia en 1983, las universidades públicas recuperaron su autonomía basada en el cogobierno y la participación de los claustros. Se reafirmó el principio de la gratuidad, el ingreso abierto y la matrícula comenzó un proceso de crecimiento acelerado. Hubo un renovado enfoque en mejorar la calidad y accesibilidad de la educación universitaria.",
+    link: "https://www.cultura.gob.ar/media/uploads/apertura_4J1d8pv.jpg",
+    type: "image",
+  },
+  {
+    id: 6,
+    date: "1990",
+    title: "Los 90 - Avances del neoliberalismo",
+    content: `Durante el período de los gobiernos de la convertibilidad son visibles las decisiones y las políticas implementadas que se corresponden con el modelo de país planteado por el menemismo, basado en propuestas que tienen su fundamento en ideas neoliberales.
+      Es así que el sistema universitario se vio afectado por una serie de
+      medidas y de determinaciones que, en varios aspectos, implicaron la re-discusión y el cuestionamiento de ciertos principios reformistas.
+      Esta etapa estuvo marcada por la falta de financiamiento a la educación tanto en el nivel medio como superior.  La educación ya no era considerada como un bien social, sino como un elemento de mercado, como un servicio que se ofrecía desde instituciones públicas o privadas.`,
+    link: "https://www.youtube.com/embed/FaK3VaQQvnA",
+    type: "video",
+  },
+  {
+    id: 7,
+    date: "2003-2015",
+    title: "Nueva era",
+    content: `Luego de la brutal crisis del 2001 y la salida anticipada de Fernando de la Rúa asume Nestro Kirchner como presidente de la nación. La política de educación superior en su mandato y los dos de Cristina Fernandez se ve caracterizada por un aumento sustancial en el presupuesto destinado a las universidades y becas. Otro aspecto relevante fue la creación de 16 casas de estudio superior. En ningún otro período de tan pocos años el sistema universitario logró dicho nivel de expansión, y en ningún otro país de la región la matrícula universitaria en universidades de gestión estatal resultó ser tan importante.
+      La creación de estas nuevas universidades modificó el mapa del sistema universitario argentino, pero principalmente generó la posibilidad de que nuevos sujetos se incorporen a un espacio de formación superior: primera generación de estudiantes universitarios, mayores de treinta años, y madres y padres son algunos de los grupos sociales que se han visto beneficiados por esta política pública de educación superior.`,
+    link: "https://www.santafe.gov.ar/noticias/media/cache/thumb_no_full/recursos/fotos/2022/07/2022-07-28NID_275432O_1.jpeg",
+    type: "image",
+  },
+  {
+    id: 8,
+    date: "2003-2015",
+    title: "Nueva era",
+    content: `La Universidad Nacional de Rafaela nace en diciembre de 2014 y rápidamente se convierte en una propuesta innovadora e interesante para estudiantes de la zona y de todo el país. En el 2016 comienzan las tres primeras carreras: Licenciatura en Diseño Industrial, Medios Audiovisuales y Digitales y Relaciones del Trabajo. En el año 2017 se presenta el Masterplan del Campus Universitario, comienzan a desarrollarse fuertemente los proyectos de extensión universitaria, programas de becas propias, acciones de internacionalización y primeras diplomaturas. Año a año la universidad continuó creciendo, incorporando propuestas y aumentando notablemente la matrícula. Así llegamos a la actualidad, con una amplia oferta de tecnicaturas, licenciaturas, ingenierías y maestrías, con más de 3600 estudiantes transitando las aulas presenciales y virtuales.`,
+    link: "https://www.youtube.com/embed/MWSbFO62_WQ",
+    type: "video",
   },
 ];
